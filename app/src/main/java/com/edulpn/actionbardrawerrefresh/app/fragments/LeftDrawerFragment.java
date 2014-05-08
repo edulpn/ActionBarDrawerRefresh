f4869e09864661e1e3f1badc3830f82a3be169a8
Eduardo Pinto <edulpn@gmail.com>
2014-05-08 01:08:24 -0300

    Cleaned up some messed up code on the drawer fragment. Also changed the ActionBar-PullToRefresh from stock to compat version.

--- app/src/main/java/com/edulpn/actionbardrawerrefresh/app/fragments/LeftDrawerFragment.java
+++ app/src/main/java/com/edulpn/actionbardrawerrefresh/app/fragments/LeftDrawerFragment.java
@@ -1,25 +1,18 @@
 package com.edulpn.actionbardrawerrefresh.app.fragments;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.edulpn.actionbardrawerrefresh.app.R;
 
-import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
-import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout;
-import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
-
 /**
  * Created by eduardo on 07/05/14.
  */
-public class LeftDrawerFragment extends ListFragment implements OnRefreshListener
+public class LeftDrawerFragment extends Fragment
 {
-    private PullToRefreshLayout mPullToRefreshLayout;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
@@ -33,48 +26,6 @@ public class LeftDrawerFragment extends ListFragment implements OnRefreshListene
     public void onViewCreated(View view, Bundle savedInstanceState)
     {
         super.onViewCreated(view,savedInstanceState);
-        ViewGroup viewGroup = (ViewGroup) view;
 
-        // As we're using a ListFragment we create a PullToRefreshLayout manually
-        mPullToRefreshLayout = new PullToRefreshLayout(viewGroup.getContext());
-
-        // We can now setup the PullToRefreshLayout
-        ActionBarPullToRefresh.from(getActivity())
-                // We need to insert the PullToRefreshLayout into the Fragment's ViewGroup
-                .insertLayoutInto(viewGroup)
-                        // Here we mark just the ListView and it's Empty View as pullable
-                .theseChildrenArePullable(android.R.id.list, android.R.id.empty)
-                .listener(this)
-                .setup(mPullToRefreshLayout);
-    }
-
-    @Override
-    public void onRefreshStarted(View view)
-    {
-        new AsyncTask<Void, Void, Void>() {
-
-            @Override
-            protected Void doInBackground(Void... params) {
-                try
-                {
-                    //Sleep de 5s
-                    Thread.sleep(5000);
-                }
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void result)
-            {
-                super.onPostExecute(result);
-
-                // Notify PullToRefreshLayout that the refresh has finished
-                mPullToRefreshLayout.setRefreshComplete();
-            }
-        }.execute();
     }
 }
